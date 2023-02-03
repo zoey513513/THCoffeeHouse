575ffe629bc0bd45aafccf061c2af2bfcc4b37ef
Yan <zyyang513@gmail.com>
2023-02-02 23:06:34 -0600

    Initial Commit

---
+++ THCoffeeHouse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  THCoffeeHouse
+//
+//  Created by yan on 2/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ THCoffeeHouse/THCoffeeHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  THCoffeeHouseApp.swift
+//  THCoffeeHouse
+//
+//  Created by yan on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct THCoffeeHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
